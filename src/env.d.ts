bcc64265eac0736f85df7ef2ca66f8aa469ba86e
Lester Romero Bernardo <lbernardo.dev@gmail.com>
2026-03-29 15:39:58 +0200

    refactor: update landing page layout, add image type declarations, and redesign footer with dark theme

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,22 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module "*.PNG" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.webp" {
+  const content: any;
+  export default content;
+}
